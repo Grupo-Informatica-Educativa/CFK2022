b999fbd36e6ab2e2bd639ccbf77e315a7f6af1a1
mariaf494 <mariaf494@gmail.com>
2022-05-27 07:41:42 -0500

    añadir favicons

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def main():
     st.set_page_config(
     layout="wide",
     page_title="CFK2021",
-    page_icon="👩‍💻")
+    page_icon="🤖")
     st.sidebar.title("CFK 2022")
     pag = st.sidebar.radio("Página: ", list(PAGES.keys()))
 
